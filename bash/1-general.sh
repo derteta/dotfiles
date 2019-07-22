4374dea5ef03748de12a78b917f106ccfad05ca3
derteta <derteta@gmx.de>
2019-07-22 22:27:18 +0200

    Add first version of the bash dotfiles

---
+++ bash/1-general.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o vi 
+export TERM=xterm-256color 
+export EDITOR=vim 
+export LC_ALL=C
+
+case $- in 
+  i ) ;; 
+  *) return;; #If not running interactively, don't do anything
+esac
+
+HISTCONTROL=ignoreboth 
+HISTSIZE=1000 
+HISTFILESIZE=2000
+shopt -s histappend 
+shopt -s checkwinsize 
+shopt -s globstar
+
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+[ -f ~/.config/fzf.sh ] && . ~/.config/fzf.sh 
+
+if ! shopt -oq posix; then 
+  for compl in /usr/share/bash-completion/bash_completion /etc/bash_completion
+  do
+    [ -f $compl ] && . $compl
+  done
+fi
---
+++ bash/2-aliases.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ -x /usr/bin/dircolors ]; then 
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)" 
+  alias ls='ls --color=auto' 
+  alias grep='grep --color=auto' 
+fi
+
+alias ll='ls -l' 
+alias la='ls -lA' 
+alias l='ls -CF'
+
+alias gn='geeknote' 
+alias e_bash='$EDITOR ~/.config/derteta/bash' 
+alias e_i3='$EDITOR ~/.config/i3/config'
+
+alias show_time='while sleep 1;do tput sc;tput cup 0 $(($(tput cols)-29));date;tput rc;done &'
+alias weather='curl wttr.in'
