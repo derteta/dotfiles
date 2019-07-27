d2e7cc704e771fa22c45f154b0b31bc21705d271
derteta <derteta@gmx.de>
2019-07-27 21:43:41 +0200

    Add inputrc

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -3,6 +3,7 @@
 repo_url=https://raw.githubusercontent.com/derteta/dotfiles/master
 bash_dir=~/.config/derteta/bash
 bashrc_path=~/.bashrc
+inputrc_path=~/.inputrc
 
 [ ! -d $bash_dir ] && mkdir -p $bash_dir
 for f in 1-general 2-aliases 3-dev
@@ -12,3 +13,6 @@ done
 
 [ -f $bashrc_path ] && cp $bashrc_path $bashrc_path.old
 echo 'for f in $(ls '$bash_dir'/*.sh); do source $f; done' > $bashrc_path
+
+[ -f $inputrc_path ] && cp $inputrc_path $inputrc_path.old
+curl -o $inputrc_path $repo_url/misc/.inputrc
