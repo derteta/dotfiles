e817aafbb29d511075685ad983542743a5bc6508
derteta <derteta@gmx.de>
2019-08-04 15:17:36 +0200

    Refactor: Extract function for backing up files

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -6,19 +6,26 @@ bashrc_path=~/.bashrc
 inputrc_path=~/.inputrc
 i3status_path=~/.config/i3status/config
 
+backup_file()
+{
+  path_to_file=$1
+  [ -f $path_to_file ] && cp $path_to_file $path_to_file.old
+}
+
+
 [ ! -d $bash_dir ] && mkdir -p $bash_dir
 for f in 1-general 2-aliases 3-dev
 do
   curl -o $bash_dir/$f.sh $repo_url/bash/$f.sh
 done
 
-[ -f $bashrc_path ] && cp $bashrc_path $bashrc_path.old
+backup_file $bashrc_path
 echo 'for f in $(ls '$bash_dir'/*.sh); do source $f; done' > $bashrc_path
 
-[ -f $inputrc_path ] && cp $inputrc_path $inputrc_path.old
+backup_file $inputrc_path
 curl -o $inputrc_path $repo_url/misc/.inputrc
 
 if [ ! -z $(uname -s | grep -i Linux) ]; then
-  [ -f $i3status_path ] && cp $i3status_path $i3status_path.old
+  backup_file $i3status_path
   curl -o $i3status_path $repo_url/i3status/config
 fi
