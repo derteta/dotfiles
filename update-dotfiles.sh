b75bb826cde05097b9018b9c8f2aae74cc7980bd
derteta <derteta@gmx.de>
2019-08-04 15:11:54 +0200

    Add i3status config

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -4,6 +4,7 @@ repo_url=https://raw.githubusercontent.com/derteta/dotfiles/master
 bash_dir=~/.config/derteta/bash
 bashrc_path=~/.bashrc
 inputrc_path=~/.inputrc
+i3status_path=~/.config/i3status/config
 
 [ ! -d $bash_dir ] && mkdir -p $bash_dir
 for f in 1-general 2-aliases 3-dev
@@ -16,3 +17,8 @@ echo 'for f in $(ls '$bash_dir'/*.sh); do source $f; done' > $bashrc_path
 
 [ -f $inputrc_path ] && cp $inputrc_path $inputrc_path.old
 curl -o $inputrc_path $repo_url/misc/.inputrc
+
+if [ ! -z $(uname -s | grep -i Linux) ]; then
+  [ -f $i3status_path ] && cp $i3status_path $i3status_path.old
+  curl -o $i3status_path $repo_url/i3status/config
+fi
