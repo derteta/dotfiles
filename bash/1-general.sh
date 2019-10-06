cb8724fa77cfca6cbc7aeb5a6211babc571a133e
derteta <derteta@gmx.de>
2019-10-06 14:00:55 +0200

    Cleanup: Remove trailing whitespaces

--- bash/1-general.sh
+++ bash/1-general.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-set -o vi 
-export TERM=xterm-256color 
-export EDITOR=vim 
+set -o vi
+export TERM=xterm-256color
+export EDITOR=vim
 export LC_ALL=C
 
-case $- in 
-  i ) ;; 
+case $- in
+  i ) ;;
   *) return;; #If not running interactively, don't do anything
 esac
 
-HISTCONTROL=ignoreboth 
-HISTSIZE=1000 
+HISTCONTROL=ignoreboth
+HISTSIZE=1000
 HISTFILESIZE=2000
-shopt -s histappend 
-shopt -s checkwinsize 
+shopt -s histappend
+shopt -s checkwinsize
 shopt -s globstar
 
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-[ -f ~/.config/fzf.sh ] && . ~/.config/fzf.sh 
+[ -f ~/.config/fzf.sh ] && . ~/.config/fzf.sh
 
-if ! shopt -oq posix; then 
+if ! shopt -oq posix; then
   for compl in /usr/share/bash-completion/bash_completion /etc/bash_completion
   do
     [ -f $compl ] && . $compl
--- bash/3-dev.sh
+++ bash/3-dev.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-alias gdb='gdb -q' # Quiet down gdb 
+alias gdb='gdb -q' # Quiet down gdb
 alias gil='git log --graph --pretty=oneline --abbrev-commit --decorate=short'
 
-gdrop_func() 
+gdrop_func()
 {
-  status="$(git status 2 > /dev/null | tail -n1)" 
-  no_changes="nothing to commit, working directory clean" 
-  if [ "$status" != "$no_changes" ]; then 
-    git stash 
-    git stash drop 
-  fi 
+  status="$(git status 2 > /dev/null | tail -n1)"
+  no_changes="nothing to commit, working directory clean"
+  if [ "$status" != "$no_changes" ]; then
+    git stash
+    git stash drop
+  fi
 }
-alias gdrop=gdrop_func 
+alias gdrop=gdrop_func
 
-alias gupstream='git push -u origin $(git rev-parse --abbrev-ref HEAD)' 
-alias goroot='relative_path="$(git rev-parse --show-cdup)"; if [ "$relative_path" != "" ]; then cd $relative_path; fi' 
-alias gmod='(goroot && git submodule update --init --recursive)' 
-alias grev='git rev-parse' 
+alias gupstream='git push -u origin $(git rev-parse --abbrev-ref HEAD)'
+alias goroot='relative_path="$(git rev-parse --show-cdup)"; if [ "$relative_path" != "" ]; then cd $relative_path; fi'
+alias gmod='(goroot && git submodule update --init --recursive)'
+alias grev='git rev-parse'
 alias gpick='git cherry-pick'
 alias grmuntracked='git st --porcelain | grep ?? | sed "s/^?? //" | sed "s/^/\"/" | sed "s/$/\"/" | xargs rm -rf'
 
