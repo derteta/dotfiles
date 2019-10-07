57676c902ba158444aa5f7af1694113c87bcab24
derteta <derteta@gmx.de>
2019-10-07 21:20:17 +0200

    linux-setup: create all necessary directories first

--- linux-setup.sh
+++ linux-setup.sh
@@ -11,6 +11,10 @@ urxvt_plugin_folder=/usr/lib/urxvt/perl
 my_github_repo="https://github.com/derteta"
 urxvt_plugin_repo="https://raw.githubusercontent.com/muennich/urxvt-perls/master"
 
+[ ! -d $dev_folder/third-party ] && mkdir -p $dev_folder/third-party
+[ ! -d $HOME/Pictures ] && mkdir -p $HOME/Pictures
+[ ! -d $urxvt_plugin_folder ] && sudo mkdir -p $urxvt_plugin_folder
+
 echo "===== Installing Development Tools ====="
 $apt_install_cmd build-essential python3 python3-pip perl git
 $pip_install_cmd nose mock
@@ -20,12 +24,10 @@ $apt_install_cmd fzf ripgrep bash-completion curl
 #TODO install geeknote
 
 echo "===== Setting up URXVT Plugins ====="
-[ ! -d $urxvt_plugin_folder ] && sudo mkdir -p $urxvt_plugin_folder
 sudo curl -o $urxvt_plugin_folder/keyboard-select $urxvt_plugin_repo/keyboard-select
 sudo curl -o $urxvt_plugin_folder/url-select $urxvt_plugin_repo/deprecated/url-select
 
 echo "===== Setting up Development Projects ====="
-[ ! -d $dev_folder/third-party ] && mkdir -p $dev_folder/third-party
 [ ! -d $dev_folder/third-party/vim ] && \
   git clone https://github.com/vim/vim.git $dev_folder/third-party/vim
 for project in vimpair dotfiles dev-blog
